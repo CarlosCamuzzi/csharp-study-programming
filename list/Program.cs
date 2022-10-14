7210bdfee45cef2f63936a2aa64df53c75283f1b
CarlosCamuzzi <carloscamuzzi.hs@gmail.com>
2022-10-14 11:10:44 -0300

    list

--- list/Program.cs
+++ list/Program.cs
@@ -21,16 +21,56 @@ Console.WriteLine($"Quantidade de Elementos na Lista: {aluno.Count} ");
 
 Console.WriteLine("Adicionando aluno com Add()");
 aluno.Add("Pedro");
+Console.WriteLine("Ordenando após inserção:");
 aluno.Sort(); // Ordenando
 ListarAlunos(aluno);
 
 //Contains(T item): retorna um valor booleano indicando se o item pesquisado existe na lista.
+Console.WriteLine("Verificando se aluno existe com Contains()");
 bool contem = aluno.Contains("Carlos");
 Console.WriteLine(contem ? "Aluno Carlos existe" : "Aluno Carlos não existe");
 
+//Insert(int index, T item): funciona semelhante ao Add, mas insere o item em uma posição definida por index.
+
+Console.WriteLine("Adicionando aluno com Insert() - Index 2");
+aluno.Insert(2, "Júlio");
+ListarAlunos(aluno);
+Console.WriteLine("Ordenando após inserção:");
+aluno.Sort();
+ListarAlunos(aluno);
+
+//InsertRange(int index, IEnumerable<T> collection): funciona semelhante ao AddRange, mas insere a nova lista na posição definida por index.
+Console.WriteLine("Inserindo com InsertRange() - Index 4" +
+                  "\nObs.: Adiciona um array");
+string[] novosAlunos = { "Ana Paula, José, Patrick" };
+aluno.InsertRange(4, novosAlunos);
+ListarAlunos(aluno);
+Console.WriteLine("Ordenando após inserção:");
+aluno.Sort();
+ListarAlunos(aluno);
+
+//Remove(T item): remove o item da lista e retorna um booleano indicando o sucesso da operação. Caso o item não seja encontrado, também retorna false.
+Console.WriteLine("Removendo lista de alunos adicionada acima com Remove()");
+bool removido = aluno.Remove("Ana Paula, José, Patrick");
+Console.WriteLine(removido ? "Alunos removidos" : "Alunos não removidos");
+ListarAlunos(aluno);
+
+// Count e Capacity
+Console.WriteLine($"Quantidade de Elementos na Lista: {aluno.Count} ");
+Console.WriteLine($"Tamanho da Lista: {aluno.Capacity}");
+
+// ToArray(): retorna um vetor do tipo contido na lista.
+Console.WriteLine("Retornando um array com os itens da lista com ToArray()");
+var arr = aluno.ToArray();
+
+Console.WriteLine("Listando alunos retornados no array");
+foreach (var nome in arr)
+{
+  Console.WriteLine($"Aluno: {nome}");
+}
 
 
-
+// Função para listar todos alunos
 void ListarAlunos(List<string> aluno)
 {
   foreach (var nome in aluno)
